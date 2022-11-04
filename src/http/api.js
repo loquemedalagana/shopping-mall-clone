9ad657e0b576c9d8df9274cf7b5be5e9870dbd72
Mrs.Liberty <nadamasquetu012@gmail.com>
2022-11-04 16:35:10 +0100

    refactored rest api code

--- src/http/api.js
+++ src/http/api.js
@@ -1,6 +1,10 @@
-import axios from 'axios';
+import _axios from 'axios';
 
 import * as restApiURL from 'src/http/restApiURL';
+
+const axios = _axios.create();
+
+axios.defaults.baseURL = restApiURL.REST_API__BASE;
 
 export const restApiProductList = async () => {
   const response = await axios.get(restApiURL.REST_API_URL__PRODUCTS);
