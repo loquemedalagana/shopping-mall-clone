9decf566affe9eb88bd8332b763dd9a8b4a36033
Mrs.Liberty <nadamasquetu012@gmail.com>
2022-11-04 19:56:39 +0100

    create storage test functions

--- cypress/e2e/app.spec.cy.js
+++ cypress/e2e/app.spec.cy.js
@@ -1,11 +1,17 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'cypress-if';
 import { URL_PRODUCTS, URL_ROOT } from '../../src/routes/routeURL';
+import { APP__CART_COUNT_KEY, APP__PRODUCT_LIST_KEY, APP_KEY } from '../../src/env';
 
 describe('visit routes', () => {
   const sampleId = 'qu-cIoRt8Y4ZeQdCuOr4l';
 
   it('visit home and redirect to /products link', () => {
     cy.visit(URL_ROOT);
-    cy.location('pathname').should('equal', `${URL_ROOT + URL_PRODUCTS}`);
+    cy.location('pathname')
+      .should('equal', `${URL_ROOT + URL_PRODUCTS}`)
+      .else()
+      .log('the connection is bad...');
   });
 
   it('visit sample item', () => {
@@ -17,5 +23,15 @@ describe('visit routes', () => {
       .contains(/products/i)
       .click();
     cy.location('pathname').should('equal', `${URL_ROOT + URL_PRODUCTS}`);
+  });
+
+  it('check local storage', () => {
+    cy.window()
+      .its('localStorage')
+      .invoke('getItem', APP_KEY)
+      .if()
+      .should('include', APP__CART_COUNT_KEY)
+      .else()
+      .log('The cart is empty');
   });
 });
--- cypress/e2e/productList.spec.cy.js
+++ cypress/e2e/productList.spec.cy.js
@@ -2,7 +2,7 @@
 import 'cypress-if';
 
 import { URL_PRODUCTS, URL_ROOT } from '../../src/routes/routeURL';
-import { APP_KEY } from '../../src/env';
+import { APP_KEY, APP__PRODUCT_LIST_KEY } from '../../src/env';
 
 describe('e2e test for product list page', () => {
   it('visit e2e test page', () => {
@@ -14,7 +14,7 @@ describe('e2e test for product list page', () => {
     cy.get('#item-loading').else().log('data is already loaded or still in the initial state');
   });
 
-  it('check local storage', () => {
-    cy.window().its('sessionStorage').invoke('getItem', APP_KEY).should('exist');
+  it('check session storage', () => {
+    cy.window().its('sessionStorage').invoke('getItem', APP_KEY).should('include', APP__PRODUCT_LIST_KEY);
   });
 });
