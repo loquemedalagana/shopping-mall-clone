91f3aae438f770018f9e6e5a7bf4f3a7dc4d64cf
Mrs.Liberty <nadamasquetu012@gmail.com>
2022-11-06 03:12:09 +0100

    get search data init when clicking home

--- src/containers/LandingContainer.jsx
+++ src/containers/LandingContainer.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
+import * as actions from 'src/actions/productListActions';
 import { URL_PRODUCTS, URL_ROOT } from 'src/routes/routeURL';
 
 const LandingContainer = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(actions.searchProductBrand(''));
+    dispatch(actions.searchProductModel(''));
+    dispatch(actions.setSearchInputError(false));
+    dispatch(actions.updateProductList());
+  }, []);
+
   return <Navigate to={URL_ROOT + URL_PRODUCTS} replace />;
 };
 
