ae4a8c932144cc4fd39f9a10db031006d270ef47
Mrs.Liberty <nadamasquetu012@gmail.com>
2022-11-07 12:14:17 +0100

    wrote app reducer test

--- src/tests/reducers/appReducer.spec.js
+++ src/tests/reducers/appReducer.spec.js
@@ -1,8 +1,22 @@
-import * as appActions from 'src/actions/appActions';
+import * as actions from 'src/actions/appActions';
 import { appReducer, initialState } from 'src/stores/appStore';
+import ProductDetail from 'src/models/ProductDetail';
+import mockedFetchedState from 'src/tests/__mocks__/mockedFetchedData';
 
 describe('app reducer test', () => {
   it('should return initial state', () => {
     expect(appReducer(undefined, { type: undefined })).toEqual(initialState);
+  });
+
+  it('to save product list', () => {
+    expect(
+      appReducer(initialState, actions.cacheProductList(mockedFetchedState.mockedItemList)).productList.data,
+    ).toEqual(mockedFetchedState.mockedItemList);
+  });
+
+  it('to save product detail', () => {
+    expect(
+      appReducer(initialState, actions.cacheProductDetail(mockedFetchedState.mockedItemDetail)).productDetail[0].data,
+    ).toEqual(new ProductDetail(mockedFetchedState.mockedItemDetail));
   });
 });
