a4d56b30835a4496da04b7eb1c5816cd820e0533
Mrs.Liberty <nadamasquetu012@gmail.com>
2022-11-07 18:36:31 +0100

    removed unusing saga

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
 
-import * as actions from 'src/actions/appActions';
 import routes from 'src/routes/routes';
 
 export const router = createBrowserRouter(routes);
 
 function App() {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(actions.readCachedProductListData());
-    dispatch(actions.readCachedProductDetailData());
-  }, []);
-
   return <RouterProvider router={router} />;
 }
 
