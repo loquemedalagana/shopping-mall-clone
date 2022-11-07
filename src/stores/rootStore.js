978fe0b9b8a573c0cb74b1aadca2aa2dfddaaef6
Mrs.Liberty <nadamasquetu012@gmail.com>
2022-11-07 10:20:29 +0100

    add dev tool

--- src/stores/rootStore.js
+++ src/stores/rootStore.js
@@ -1,6 +1,7 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
 
+import { isTest, isDev } from 'src/env';
 import { appReducer } from 'src/stores/appStore';
 import { productListReducer } from 'src/stores/productListStore';
 import { productDetailReducer } from 'src/stores/productDetailStore';
@@ -18,6 +19,7 @@ const rootReducer = combineReducers({
 
 const rootStore = configureStore({
   reducer: rootReducer,
+  devTools: isDev() || isTest(),
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware({ thunk: false, serializableCheck: false }).prepend(sagaMiddleWare),
 });
